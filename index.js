d1fa20a1bd1d2488ec7bd8714a7f97ea441631d6
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-05-12 08:24:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g ,"");
+  }
+
+  static titleize(string) {
+    const stringArray = string.split(` `);
+    const newStringArray = stringArray.map(word => {
+      if ((word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from") && word !== string[0]) {
+        return word;
+      } else {
+        return word[0].toUpperCase() + word.slice(1);
+      }
+    });
+    return newStringArray.join(" ");
+  }
 }
